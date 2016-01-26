dcca612a4418ab1ca1b3206a7425e32d97f45ba6
Evan Dorsky <evan.dorsky@icloud.com>
2016-01-26 17:15:56 -0500

    Beginnings of DC motor driver library

---
+++ lib/md.c
@@ -0,0 +1,62 @@
+/*
+** Copyright (c) 2016, Evan Dorsky
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met: 
+** 
+**     1. Redistributions of source code must retain the above copyright 
+**        notice, this list of conditions and the following disclaimer. 
+**     2. Redistributions in binary form must reproduce the above copyright 
+**        notice, this list of conditions and the following disclaimer in the 
+**        documentation and/or other materials provided with the distribution. 
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+** POSSIBILITY OF SUCH DAMAGE.
+*/
+#include <p24FJ128GB206.h>
+#include "common.h"
+#include "md.h"
+
+void init_md(void) {
+    md_init()
+}
+
+// dir == 0 means
+// pin[0] is active, pin[1] is 0
+// dir == 1 means
+// pin[0] is 0, pin[1] is active
+void md_init(_MD *self) {
+    self->m1dir = 0;
+    self->m1speed = 0;
+    self->m1freq = 0;
+    self->m1pins[0] = 7;
+    self->m1pins[1] = 8;
+
+    self->m2dir = 0;
+    self->m2speed = 0;
+    self->m2freq = 0;
+    self->m2pins[0] = 5;
+    self->m2pins[1] = 6;
+}
+
+void md_free(_OC *self) {
+    // clear owned pins/oc's
+}
+
+void md_speed(_MD *self, uint16_t motor, uint16_t speed) {
+    if (motor == 1) {
+        self->m1speed = speed;
+
+        m1pins[m1dir] = 1;
+    }
+}
---
+++ lib/md.h
@@ -0,0 +1,55 @@
+/*
+** Copyright (c) 2016, Evan Dorsky
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met: 
+** 
+**     1. Redistributions of source code must retain the above copyright 
+**        notice, this list of conditions and the following disclaimer. 
+**     2. Redistributions in binary form must reproduce the above copyright 
+**        notice, this list of conditions and the following disclaimer in the 
+**        documentation and/or other materials provided with the distribution. 
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+** POSSIBILITY OF SUCH DAMAGE.
+*/
+#ifndef _MD_H_
+#define _MD_H_
+
+#include <stdint.h>
+#include "pin.h"
+#include "oc.h"
+#include "timer.h"
+
+void init_md(void);
+
+typedef struct {
+    uint16_t m1dir;
+    uint16_t m1speed;
+    uint16_t m1freq;
+    uint16_t m1pins[2];
+
+    uint16_t m2dir;
+    uint16_t m2speed;
+    uint16_t m2freq;
+    uint16_t m2pins[2];
+} _MD;
+
+void md_init(_MD *self);
+void md_free(_MD *self);
+
+void md_speed(_MD *self, uint16_t motor, uint16_t speed);
+void md_direction(_MD *self, uint16_t motor, uint16_t dir);
+void md_velocity(_MD *self, uint16_t motor, uint16_t speed, uint16_t dir);
+
+#endif
