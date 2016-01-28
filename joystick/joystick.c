0fdba0f939a2ec1c65636df44ba87c6736130f2a
Anne Wilkinson <anne.wilkinson@students.olin.edu>
2016-01-28 14:16:15 -0500

    First attempt at SPI. Code to print to computer

--- joystick/joystick.c
+++ joystick/joystick.c
@@ -1,10 +1,14 @@
 #include <p24FJ128GB206.h>
+#include <stdio.h>
+#include "uart.h"
 #include "config.h"
 #include "common.h"
 #include "ui.h"
 #include "timer.h"
 #include "spi.h"
 #include "pin.h"
+#include "oc.h"
+
 
 //SPI pins
 // #define MOSI 0
@@ -17,25 +21,33 @@ int16_t main(void) {
     init_clock();
     init_ui();
     init_spi();
-    float freq = 9000;
-    // spi_open(&spi1, &MISO, &MOSI, &SCLK, freq);
-    spi_open(&spi1, &D[0], &D[1], &D[2], freq);
-    //Transfer command to zero the system.
-    spi_transfer(&spi1, )
-    //Transfer command to read angle
-    spi_transfer(&spi1, 0x3FFF)
+    init_oc();
+    init_pin();
+    init_uart();
 
-    led_on(&led1);
-    timer_setPeriod(&timer2, 0.5);
-    timer_start(&timer2);
+    pin_digitalOut(&D[3]); //set chip select as dig out
+    pin_set(&D[3]); //set chip select high
+    float freq = 1e6; //SCLK frequency
+    spi_open(&spi1, &D[0], &D[1], &D[2], freq); //open SPI
+    pin_clear(&D[3]); //set CS low
+    spi_transfer(&spi1, 0x3F);//transfer address to read
+    spi_transfer(&spi1, 0xFF);
+    pin_set(&D[3]);
+    pin_clear(&D[3]);
 
-    while (1) {
-        if (timer_flag(&timer2)) {
-            timer_lower(&timer2);
-            led_toggle(&led1);
-            led_toggle(&led2);
-        }
-        led_write(&led3, !sw_read(&sw3));
-    }
+    WORD temp;
+    temp.b[1] = spi_transfer(&spi1, 0x00); //get angle info
+    temp.b[0] = spi_transfer(&spi1, 0x00);
+
+    pin_set(&D[3]);
+    // printf("%i\n",temp.i);
+    char *string = "hello processor";
+    printf("%s\n",string);
+
+
+    ////Begin motor controller stuff
+
+
+
 }
 
