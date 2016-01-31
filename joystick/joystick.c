55993fdba80daf303e527b514a422a46c264609e
Anne Wilkinson <anne.wilkinson@students.olin.edu>
2016-01-31 17:02:50 -0500

    joystick motor code, SPI commented out

--- joystick/joystick.c
+++ joystick/joystick.c
@@ -24,30 +24,63 @@ int16_t main(void) {
     init_oc();
     init_pin();
     init_uart();
+    init_timer();
 
-    pin_digitalOut(&D[3]); //set chip select as dig out
-    pin_set(&D[3]); //set chip select high
-    float freq = 1e6; //SCLK frequency
-    spi_open(&spi1, &D[0], &D[1], &D[2], freq); //open SPI
-    pin_clear(&D[3]); //set CS low
-    spi_transfer(&spi1, 0x3F);//transfer address to read
-    spi_transfer(&spi1, 0xFF);
-    pin_set(&D[3]);
-    pin_clear(&D[3]);
+    // oc_pwm(&oc1, &D[8], &timer2, 2000, .9);
+    
+    pin_digitalOut(&D[6]);
+    pin_digitalOut(&D[7]);
+    pin_digitalOut(&D[8]);
+    pin_digitalOut(&D[5]);
 
-    WORD temp;
-    temp.b[1] = spi_transfer(&spi1, 0x00); //get angle info
-    temp.b[0] = spi_transfer(&spi1, 0x00);
-
-    pin_set(&D[3]);
-    // printf("%i\n",temp.i);
-    char *string = "hello processor";
-    printf("%s\n",string);
+    while (1){
+    led_on(&led1);//LED to tell if it is in run mod
+    pin_set(&D[6]);
+    pin_set(&D[7]);
+    pin_clear(&D[8]);
+    pin_clear(&D[5]);
+}
 
 
-    ////Begin motor controller stuff
 
 
+    ///SPI not working ----------------------
+
+    // led_on(&led1);//LED to tell if it is in run mode
+
+    // timer_setPeriod(&timer2, 0.5);
+    // timer_start(&timer2);
+    
+    
+    // pin_digitalOut(&D[3]); //set chip select as dig out
+    // pin_set(&D[3]); //set chip select high
+    // float freq = 1e6; //SCLK frequency
+    // spi_open(&spi1, &D[0], &D[1], &D[2], freq); //open SPI
+    // pin_clear(&D[3]); //set CS low
+    // spi_transfer(&spi1, 0x3F);//transfer address to read
+    // spi_transfer(&spi1, 0xFF);
+
+    // // pin_set(&D[3]);
+    
+
+
+    // while (1) {
+    //     if (timer_flag(&timer2)) {
+    //         pin_set(&D[3]);
+    //         pin_clear(&D[3]);
+    //         timer_lower(&timer2);
+    //         // char string[8] = "hello\n";
+    //         // printf("%31s\n",string);
+    //         WORD temp;
+    //         temp.b[1] = spi_transfer(&spi1, 0x00); //get angle info
+    //         temp.b[0] = spi_transfer(&spi1, 0x00);
+    //         printf("%i\n",temp.i);
+    //         // uart_puts(&uart1, string);
+    //     }
+    // }
+    // ////Begin motor controller stuff
+
+    ///End SPI not working ----------------------------
 
 }
 
